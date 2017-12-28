8870969af17fbad5f8d07c1175505cacdc42e6f1
Sergey Vasilenko <32385430+vasilenko-s@users.noreply.github.com>
2017-12-28 16:59:24 +0200

    Update #2lengthOfLastWord.php

--- #2lengthOfLastWord.php
+++ #2lengthOfLastWord.php
@@ -1,4 +1,14 @@
 <?php
+/*
+# Exersice 2
+Реализуйте функцию lengthOfLastWord, которая возвращает длину последнего слова переданной на вход строки. Словом считается любая последовательность не содержащая пробелов.
+
+Примеры:
+
+0 == lengthOfLastWord('');
+5 == lengthOfLastWord('man in BlacK');
+6 == lengthOfLastWord('hello, world!  ');
+*/
 
 namespace App\Solution;
 
